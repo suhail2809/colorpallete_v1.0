f24039410a7fc231044637d542e860e459991997
suhail2809 <mdsuhail0703@gmail.com>
2026-07-07 12:09:47 +0530

    made by color pallete generator

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+const characters="0123456789ABCDEF";
+
+
+function generateColors(){
+    let color="#";
+    
+    for(let i=0; i<6; i++){
+        let randomIndex = Math.floor(Math.random()*characters.length);
+        color+=characters[randomIndex];
+
+    }
+    return color;
+
+}
+
+function setcolor(){
+    for (let i=1; i<=5; i++){
+        let color = generateColors();
+
+        let box= document.getElementById("box"+i);
+        box.style.backgroundColor=color;
+
+        box.textContent= color;
+
+    }
+}
+
+
+
+
